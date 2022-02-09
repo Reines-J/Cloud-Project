fd5255132f6e72baf0d74d00a4dce7d967801e6c
Reines-J <90921785+Reines-J@users.noreply.github.com>
2022-02-09 11:17:58 +0900

    keypair

--- EKS/keypair.tf
+++ EKS/keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terra-key" {
     key_name = "terra-key"
-    public_key = file("/mnt/c/Users/khung/Desktop/terraform/terra-key.pub")
+    public_key = file("/mnt/c/Users/reines/Desktop/terraform/terra-key.pub")
 }
